958f9d25604fabe4d1152bd0f6415c0ceaabf107
B0204071 <sanjeev.guglani@airtelbank.com>
2018-09-21 22:51:27 +0530

    Insertion Sort

---
+++ src/com/sanjeev/InsertionSort.java
@@ -0,0 +1,27 @@
+package com.sanjeev;
+
+import java.util.Arrays;
+
+/**
+ * Created by Sanjeev Guglani
+ */
+public class InsertionSort {
+    public static void insert(int arr[])
+    {
+        for(int i=0;i<arr.length-1;i++){
+            int j=i+1;
+            int temp = arr[j];
+            while(j>0 && temp<arr[j-1]){
+                arr[j]=arr[j-1];
+                j--;
+            }
+            arr[j]=temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[]=new int[]{5,4,3,2,1};
+        insert(arr);
+        System.out.println(arr);
+    }
+}
